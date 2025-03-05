9cd2957b77dd98931e32e788d2f66f3383f7c4aa
Pratik <prateekraiger123@gmail.com>
2025-03-05 20:21:50 +0530

    added routes

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-betweeen py-5 font-medium"></div>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/Placeorder.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Placeorder = () => {
+  return <div>Placeorder</div>;
+};
+
+export default Placeorder;
