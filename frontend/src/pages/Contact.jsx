dbc98ef4efd2ef1f23d20131bc6d03977422d146
Pratik <prateekraiger123@gmail.com>
2025-03-14 08:34:30 +0530

    contacts page updated

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Title from "../components/Title";
+import assets from "../assets/assets";
 
 const Contact = () => {
   return (
     <div>
       <div className="text-center text-2xl pt-10 border-t">
         <Title text1={"CONTACT "} text2={"Us"} />
+      </div>
+
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img src={assets.about_img} alt="" className="w-full md:w" />
       </div>
     </div>
   );
